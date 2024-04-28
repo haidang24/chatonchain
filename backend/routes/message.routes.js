67b5d9f23b243e27626d15a2e9fd4891a03bbd8c
Hải Đăng <AT19N0105@actvn.edu.vn>
2024-04-28 07:12:54 +0700

    Update message.routes.js

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -2,26 +2,23 @@ import express from "express";
 import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 import { call } from "../controllers/call.controller.js";
+import multer from "multer";
+import bodyParser from "body-parser";
+
 // import  upload  from "../middleware/upload.js";
 //TESTING
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-         cb( null, './uploads/' );
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
-
+// configure storage
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, '');
+    }
+} );
+const upload = multer({ storage }).single('file');
 //
 const router = express.Router();
 
 router.get("/:id", protectRoute,getMessages);
-router.post( "/send/:id", protectRoute, upload.single( 'file' ), sendMessage );
+router.post( "/send/:id", protectRoute, upload, sendMessage );
 router.post( "/call/:id", protectRoute,)
 // router.post("/send/:id", protectRoute,sendMessage);//TEST
 export default router;
